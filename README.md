9a208591416938d8bf80a39a65335b1f49048001
Muhammad Alfarozi <mr.alfarozy.a.n@gmail.com>
2021-12-19 12:35:08 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# alfarozy.id
+
+My Official portfolio
+
+## Demo
+
+- https://me.alfarozy.id
+- https://alfarozy.github.io
+
+## To do
+
+- [ ] Design Blog.
+- [ ] Develope Backend
+- [ ] SEO
+
+## library
+
+- AOS
+- Magnific Popup
+- Isotope js
+- jquery
+- fontawsome
+
+## Design by me
+
+- Website: [Alfarozy.id](https://alfarozy.id)
+- Email : [mr.alfarozy.a.n@gmail.com](mailto:mr.alfarozy.a.n@gmail.com)
+- Fb : [Alfarozy AN](https://www.facebook.com/Alfarozy.A.n/)
+- Ig : [alfarozy_an](instagram.com/alfarozy_an/)
+
+FOR REFERENCE ONLY. If you want to see this code, please no duplicate, no copy-paste. Or you gonna get plagiarism and get the problem. Learn from scratch is the best way to being an expert, not care what it is. Thank you
---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  AOS.init();
+  $(".project .categories button").click((e) => {
+    $(".project .categories button").removeClass("active");
+    e.target.classList.add("active");
+    $(this).addClass("active");
+    let selector = $(e.target).data("filter");
+    if (selector != "*") {
+      selector = "." + selector;
+    }
+    console.log(selector);
+    $(".project .grid").isotope({
+      filter: selector,
+    });
+
+    return false;
+  });
+
+  //   magnify pop up
+
+  $(".project-link").magnificPopup({
+    type: "image",
+    gallery: { enabled: true },
+  });
+
+  var header = $(".header").height() + 20;
+  if ($(".header").length) {
+    $(window).scroll(function () {
+      let scroll = $(window).scrollTop();
+      if (scroll >= header) {
+        $(".header .main-menu").addClass("ji-navbar_fixed");
+      } else {
+        $(".header .main-menu").removeClass("ji-navbar_fixed");
+      }
+    });
+  }
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $(".scroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+});
